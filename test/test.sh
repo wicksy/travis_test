3dd446256c14c42c4448321e9d37476689ed0173
Martin Wicks <wicksy@wicksy.com>
2016-10-02 16:21:38 +0100

    Fixes to build

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mkdir -p /srv
+ln -sf "$(pwd)/laptop-build/salt/roots /srv/salt"
+ln -sf "$(pwd)/laptop-build/salt/pillar /srv/pillar"
+find /srv -follow -ls
+
 salt-call --version
 
 exit 0
