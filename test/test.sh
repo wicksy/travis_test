0da28d4d547b81ce6ab430f448b572aaa04db7cb
Martin Wicks <wicksy@wicksy.com>
2016-10-02 16:55:22 +0100

    Fix bug in sym links

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 mkdir -p /srv
-ln -sf "$(pwd)/laptop-build/salt/roots /srv/salt"
-ln -sf "$(pwd)/laptop-build/salt/pillar /srv/pillar"
+ln -sf "$(pwd)/laptop-build/salt/roots" /srv/salt
+ln -sf "$(pwd)/laptop-build/salt/pillar" /srv/pillar
 find /srv -follow -ls
 
 salt-call --version
