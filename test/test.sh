b4f188766db84f9522553e357906f8e9d709ab7f
Martin Wicks <wicksy@wicksy.com>
2016-10-30 11:05:19 +0000

    See where testinfra lives post install:

--- test/test.sh
+++ test/test.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -x
 
-mkdir -p /srv
-ln -sf "$(pwd)/CV/salt/roots" /srv/salt
-ln -sf "$(pwd)/CV/salt/pillar" /srv/pillar
+#mkdir -p /srv
+#ln -sf "$(pwd)/CV/salt/roots" /srv/salt
+#ln -sf "$(pwd)/CV/salt/pillar" /srv/pillar
 
-salt-call --local -l debug state.apply
+#salt-call --local -l debug state.apply
 
-docker ps
-sleep 30
-curl "http://localhost:8080"
+#docker ps
+#sleep 30
+#curl "http://localhost:8080"
 
-salt-call --version
+#salt-call --version
+
+echo "Home dir stuff"
+find ${HOME} -name testinfra -ls
+echo "PATH: ${PATH}"
+echo "Local bin"
+ls -l /usr/local/bin
 
 exit 0
