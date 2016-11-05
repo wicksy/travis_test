932ce15846f8af725c10ee8fef22effc7034210d
Martin Wicks <wicksy@wicksy.com>
2016-11-05 15:14:32 +0000

    Test out Ansible install

--- test/test.sh
+++ test/test.sh
@@ -1,22 +1,9 @@
 #!/bin/bash -x
 
-#mkdir -p /srv
+mkdir -p /srv
 #ln -sf "$(pwd)/CV/salt/roots" /srv/salt
 #ln -sf "$(pwd)/CV/salt/pillar" /srv/pillar
 
-#salt-call --local -l debug state.apply
-
-#docker ps
-#sleep 30
-#curl "http://localhost:8080"
-
-#salt-call --version
-
-#echo "Home dir stuff"
-#find ${HOME} -name testinfra -ls
-
-path="${1}"
-echo ${path}
-${path} --version
+ansible-playbook --version
 
 exit 0
