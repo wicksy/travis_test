5d727edc3e5581c0f56f76c4eacced9fb51047e9
Martin Wicks <wicksy@wicksy.com>
2016-11-06 11:54:46 +0000

    Try playbooks on laptop-build repo

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 
 mkdir -p /srv
-#ln -sf "$(pwd)/CV/salt/roots" /srv/salt
-#ln -sf "$(pwd)/CV/salt/pillar" /srv/pillar
+ln -sf "$(pwd)/laptop-build/salt/roots" /srv/salt
+ln -sf "$(pwd)/laptop-build/salt/pillar" /srv/pillar
+ln -sf "$(pwd)/laptop-build/ansible" /srv/ansible
+
+cd /srv/ansible && /usr/bin/ansible-playbook -i hosts site.yml -vv --limit=travis-ci
 
 ansible-playbook --version
 
