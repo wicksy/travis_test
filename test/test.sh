1bb1504ec7d337d45da03d3a2c47bad9fe14432a
Martin Wicks <wicksy@wicksy.com>
2017-04-21 06:41:44 +0100

    Troubleshooting Travis CI issues

--- test/test.sh
+++ test/test.sh
@@ -5,8 +5,11 @@ ln -sf "$(pwd)/laptop-build/salt/roots" /srv/salt
 ln -sf "$(pwd)/laptop-build/salt/pillar" /srv/pillar
 ln -sf "$(pwd)/laptop-build/ansible" /srv/ansible
 
-cd /srv/ansible && /usr/bin/ansible-playbook -i hosts site.yml -vv --limit=travis-ci
+#cd /srv/ansible && /usr/bin/ansible-playbook -i hosts site.yml -vv --limit=travis-ci
+#ansible-playbook --version
 
-ansible-playbook --version
+lsb_release -a
+echo
+cat /etc/os-release
 
 exit 0
