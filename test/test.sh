1dc6f11470d98c09c50213dde709186922de127d
Martin Wicks <wicksy@wicksy.com>
2017-04-22 05:52:14 +0100

    Troubleshoot docker repo issues

--- test/test.sh
+++ test/test.sh
@@ -5,11 +5,8 @@ ln -sf "$(pwd)/laptop-build/salt/roots" /srv/salt
 ln -sf "$(pwd)/laptop-build/salt/pillar" /srv/pillar
 ln -sf "$(pwd)/laptop-build/ansible" /srv/ansible
 
-#cd /srv/ansible && /usr/bin/ansible-playbook -i hosts site.yml -vv --limit=travis-ci
-#ansible-playbook --version
-
-lsb_release -a
-echo
-cat /etc/os-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "deb https://download.docker.com/linux/ubuntu trusty stable" > /etc/apt/sources.list.d/docker.list
+apt-get update
 
 exit 0
