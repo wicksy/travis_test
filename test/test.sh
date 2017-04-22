59972a6ba06498073d0a589fc0b600c392e5c956
Martin Wicks <wicksy@wicksy.com>
2017-04-22 05:55:38 +0100

    Troubleshoot docker repo issues

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@ ln -sf "$(pwd)/laptop-build/salt/pillar" /srv/pillar
 ln -sf "$(pwd)/laptop-build/ansible" /srv/ansible
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo "deb https://download.docker.com/linux/ubuntu trusty stable" > /etc/apt/sources.list.d/docker.list
+echo "deb http://download.docker.com/linux/ubuntu trusty stable" > /etc/apt/sources.list.d/docker.list
 apt-get update
 
 exit 0
