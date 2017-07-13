744bcceb6ed5cfe2fa670759eda5025b14f9d341
Martin Wicks <wicksy@wicksy.com>
2017-07-13 07:28:42 +0100

    Try out python2 and python3 together

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -x
 
-ln -sf "$(pwd)/configfiles/dotfiles/.vimrc" "${HOME}/.vimrc"
-mkdir -p "${HOME}/.vim/bundle"
-cd "${HOME}/.vim/bundle"
-git clone https://github.com/VundleVim/Vundle.vim.git
-cat "${HOME}/.vimrc"
-ls -ail "${HOME}"
-ls -ail "${HOME}/.vim/bundle"
-vim +PluginInstall +qall
+pip --version
+pip2 --version
+pip3 --version
+python --version
+python2 --version
+python3 --version
 
 exit 0
