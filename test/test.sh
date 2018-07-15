740814134cba6ac26a2d689f6bd17eff53140065
Martin Wicks <wicksy@wicksy.com>
2018-07-15 17:16:08 +0100

    Fix broken link

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 mkdir -p /srv
-ln -sf "$(pwd)/ansible" /srv/ansible
+ln -sf "$(pwd)/laptop-build/ansible" /srv/ansible
 
 export ANSIBLE_NOCOWS=1
 cd /srv/ansible && /usr/bin/ansible-playbook -i hosts site.yml --limit=localhost
