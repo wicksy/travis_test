ddfb81bcce06e51860f3fff602778fe40088cf20
Martin Wicks <wicksy@wicksy.com>
2018-07-15 16:31:01 +0100

    See what version of ansible is being installed

--- test/test.sh
+++ test/test.sh
@@ -1,19 +1,11 @@
 #!/bin/bash -x
 
 pip --version
-#pip2 --version
+pip2 --version
 pip3 --version
 python --version
 python2 --version
 python3 --version
-
-for i in pip pip2 pip3 python python2 python3
-do
-  echo ${i}
-  which ${i}
-done
-
-dpkg -L python3-pip
-find /home/travis/virtualenv/ -ls
+ansible --version
 
 exit 0
