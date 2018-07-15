bde82b87b6f303ab3276ad74f1637ac8720172f4
Martin Wicks <wicksy@wicksy.com>
2018-07-15 17:36:11 +0100

    Fix ordering

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
+mkdir -p /srv
+ln -sf "$(pwd)/laptop-build/ansible" /srv/ansible
+
 cat > /srv/ansible/hosts << EOF
 [localhost]
 localhost ansible_connection=local
 EOF
-
-mkdir -p /srv
-ln -sf "$(pwd)/laptop-build/ansible" /srv/ansible
 
 export ANSIBLE_NOCOWS=1
 cd /srv/ansible && /usr/bin/ansible-playbook -i hosts site.yml --limit=localhost
